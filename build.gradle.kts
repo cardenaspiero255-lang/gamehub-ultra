581b8d3f8c16dbde575856f1d6115398d4219d22
Piero <cardenaspiero255@gmail.com>
2026-09-22 20:49:30 -0300

    build: align Kotlin with ML Kit GenAI metadata

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,6 @@ plugins {
     id("com.android.application") version "8.10.1" apply false
     id("com.android.test") version "8.10.1" apply false
     id("androidx.baselineprofile") version "1.5.0" apply false
-    id("org.jetbrains.kotlin.android") version "2.0.21" apply false
-    id("org.jetbrains.kotlin.plugin.compose") version "2.0.21" apply false
+    id("org.jetbrains.kotlin.android") version "2.3.21" apply false
+    id("org.jetbrains.kotlin.plugin.compose") version "2.3.21" apply false
 }
