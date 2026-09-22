637380ef55dbfb2828484f9404919b19fef7772e
Piero <cardenaspiero255@gmail.com>
2026-09-22 17:57:08 -0300

    fix: use Android SuppressLint annotation

--- app/src/main/java/com/cardenaspiero255/gamehubultra/MainActivity.kt
+++ app/src/main/java/com/cardenaspiero255/gamehubultra/MainActivity.kt
@@ -7,7 +7,7 @@ import android.content.IntentFilter
 import android.os.Build
 import android.os.Bundle
 import android.os.PowerManager
-import androidx.annotation.SuppressLint
+import android.annotation.SuppressLint
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Arrangement
