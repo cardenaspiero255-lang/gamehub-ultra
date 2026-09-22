ca9b331c1728474ff2d11fbab64a259d2b191ec3
Piero <cardenaspiero255@gmail.com>
2026-09-22 17:13:41 -0300

    test: cover hardware detection parser

--- app/src/test/java/com/cardenaspiero255/gamehubultra/domain/PerformanceProfileTest.kt
+++ app/src/test/java/com/cardenaspiero255/gamehubultra/domain/PerformanceProfileTest.kt
@@ -1,8 +1,10 @@
 package com.cardenaspiero255.gamehubultra.domain
 
+import com.cardenaspiero255.gamehubultra.platform.CpuInfoParser
 import kotlin.test.Test
 import kotlin.test.assertEquals
 import kotlin.test.assertFalse
+import kotlin.test.assertNull
 import kotlin.test.assertTrue
 
 class PerformanceProfileTest {
@@ -17,5 +19,27 @@ class PerformanceProfileTest {
     fun interpolationProfileDoesNotClaimUnsupportedControl() {
         assertTrue(PerformanceProfile.FRAME_INTERPOLATION.description.contains("No puede forzar"))
         assertFalse(PerformanceProfile.FRAME_INTERPOLATION.description.contains("activa interpolación"))
+    }
+
+    @Test
+    fun parsesCpuModelFromCommonLinuxFormats() {
+        val info = """
+            processor : 0
+            model name : ARMv8 Processor rev 4 (v8l)
+            Hardware : Generic Device
+        """.trimIndent()
+
+        assertEquals("ARMv8 Processor rev 4 (v8l)", CpuInfoParser.parseModel(info))
+    }
+
+    @Test
+    fun parsesHardwareWhenModelNameIsMissing() {
+        val info = "Hardware : Qualcomm Technologies, Inc. SM8650"
+        assertEquals("Qualcomm Technologies, Inc. SM8650", CpuInfoParser.parseModel(info))
+    }
+
+    @Test
+    fun returnsNullWhenCpuInfoHasNoKnownModelKey() {
+        assertNull(CpuInfoParser.parseModel("processor : 0\nflags : neon"))
     }
 }
