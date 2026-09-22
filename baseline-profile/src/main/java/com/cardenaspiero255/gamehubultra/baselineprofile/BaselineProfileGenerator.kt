d6ea4f563459cab66269c397aba4099eb6c1758e
Piero <cardenaspiero255@gmail.com>
2026-09-22 19:32:44 -0300

    perf: profile startup and core navigation

---
+++ baseline-profile/src/main/java/com/cardenaspiero255/gamehubultra/baselineprofile/BaselineProfileGenerator.kt
@@ -0,0 +1,27 @@
+package com.cardenaspiero255.gamehubultra.baselineprofile
+
+import androidx.benchmark.macro.junit4.BaselineProfileRule
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.LargeTest
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+@LargeTest
+class BaselineProfileGenerator {
+    @get:Rule
+    val baselineProfileRule = BaselineProfileRule()
+
+    @Test
+    fun startupAndCoreNavigation() = baselineProfileRule.collect(
+        packageName = "com.cardenaspiero255.gamehubultra"
+    ) {
+        startActivityAndWait()
+        device.waitForIdle()
+
+        device.pressHome()
+        startActivityAndWait()
+        device.waitForIdle()
+    }
+}
