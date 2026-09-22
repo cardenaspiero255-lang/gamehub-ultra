493afab97c0b8be18fa4fb0b78347ef2bc484c1f
Piero <cardenaspiero255@gmail.com>
2026-09-22 18:30:15 -0300

    feat: add CAR-12 voice assistant component

---
+++ app/src/test/java/com/cardenaspiero255/gamehubultra/voice/VoiceInteractionProtocolTest.kt
@@ -0,0 +1,18 @@
+package com.cardenaspiero255.gamehubultra.voice
+
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class VoiceInteractionProtocolTest {
+    @Test
+    fun commandIdentifierIsStable() {
+        assertEquals(
+            "com.cardenaspiero255.gamehubultra.EXECUTE_VOICE_COMMAND",
+            GameHubVoiceInteractionSessionService.COMMAND_EXECUTE_TEXT
+        )
+    }
+
+    @Test
+    fun transcriptExtraIdentifierIsStable() {
+        assertEquals("transcript", GameHubVoiceInteractionSessionService.EXTRA_TRANSCRIPT)
+    }
