c597eabff6411b33b3932c13bf17bc8d57c2fe95
Piero <cardenaspiero255@gmail.com>
2026-09-22 22:27:54 -0300

    fix(car-16): remove unavailable LargeTest filter from baseline profile

--- baseline-profile/src/main/java/com/cardenaspiero255/gamehubultra/baselineprofile/BaselineProfileGenerator.kt
+++ baseline-profile/src/main/java/com/cardenaspiero255/gamehubultra/baselineprofile/BaselineProfileGenerator.kt
@@ -2,7 +2,6 @@ package com.cardenaspiero255.gamehubultra.baselineprofile
 
 import androidx.benchmark.macro.junit4.BaselineProfileRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
-import androidx.test.filters.LargeTest
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.uiautomator.By
 import androidx.test.uiautomator.Until
@@ -11,7 +10,6 @@ import org.junit.Test
 import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
-@LargeTest
 class BaselineProfileGenerator {
     @get:Rule
     val baselineProfileRule = BaselineProfileRule()
