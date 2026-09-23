83e8afca4003b660204a347647af150bae740909
Piero <cardenaspiero255@gmail.com>
2026-09-23 17:07:20 -0300

    refactor: centralize CAR-18 compact UI tokens

---
+++ app/src/main/java/com/cardenaspiero255/gamehubultra/ui/theme/GameHubUiTokens.kt
@@ -0,0 +1,33 @@
+package com.cardenaspiero255.gamehubultra.ui.theme
+
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+/**
+ * Compact GameHub Ultra visual tokens.
+ *
+ * Keeping the compact spacing and typography in one place prevents UI
+ * regressions that make the landscape dashboard unnecessarily oversized.
+ */
+object GameHubUiTokens {
+    val compactHorizontalPadding = 12.dp
+    val compactSectionSpacing = 10.dp
+    val compactCardPadding = 14.dp
+    val compactControlSpacing = 8.dp
+
+    val displayLarge = 34.sp
+    val displayMedium = 28.sp
+    val displaySmall = 24.sp
+    val headlineLarge = 24.sp
+    val headlineMedium = 21.sp
+    val headlineSmall = 20.sp
+    val titleLarge = 18.sp
+    val titleMedium = 15.sp
+    val titleSmall = 14.sp
+    val bodyLarge = 14.sp
+    val bodyMedium = 13.sp
+    val bodySmall = 12.sp
+    val labelLarge = 12.sp
+    val labelMedium = 11.sp
+    val labelSmall = 10.sp
+}
