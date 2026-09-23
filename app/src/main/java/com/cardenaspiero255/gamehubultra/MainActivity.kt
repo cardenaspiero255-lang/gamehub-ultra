0c3789be5be62be6525ab1678bbb4a6fa0094b2a
Piero <cardenaspiero255@gmail.com>
2026-09-22 21:36:21 -0300

    feat: add Ultra chat field

--- app/src/main/java/com/cardenaspiero255/gamehubultra/MainActivity.kt
+++ app/src/main/java/com/cardenaspiero255/gamehubultra/MainActivity.kt
@@ -35,6 +35,7 @@ import androidx.compose.material3.Card
 import androidx.compose.material3.LinearProgressIndicator
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
