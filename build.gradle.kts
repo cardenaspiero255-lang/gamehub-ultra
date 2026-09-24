626ce12edc80ca03785ca652b78f229c6ec38fdd
Piero <cardenaspiero255@gmail.com>
2026-09-24 09:57:23 -0300

    fix(CAR-29): align AGP R8 with Kotlin 2.3

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-    id("com.android.application") version "8.10.1" apply false
-    id("com.android.test") version "8.10.1" apply false
+    id("com.android.application") version "8.13.2" apply false
+    id("com.android.test") version "8.13.2" apply false
     id("androidx.baselineprofile") version "1.5.0" apply false
     id("org.jetbrains.kotlin.android") version "2.3.21" apply false
     id("org.jetbrains.kotlin.plugin.compose") version "2.3.21" apply false
